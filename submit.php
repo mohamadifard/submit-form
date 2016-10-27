ea26b39eda4af2182827df588dd703b59ce89465
mohamadifard <motahare512@gmail.com>
2016-10-27 06:36:24 +0330

    edit submit form

--- submit.php
+++ submit.php
@@ -2,24 +2,24 @@
 
 		echo '<center><h3>Wellcom in this page we test get and post method</h3></center> <br>';
 		echo '<h4>How this page work ?</h4><br> Use This Url <h5>http://[WWW.submit form]/Post.php?</h5><br>';
-		echo 'Get   method First name ==> '.$_POST["fname"].'!<br>';
-		echo 'Get  method Last name ==> '.$_POST["lname"].'!<br>';
-		echo 'Get   method Birthday ==> '.$_POST["date"].'!<br>';
+		echo 'Post   method First name ==> '.$_POST["fname"].'!<br>';
+		echo 'Post  method Last name ==> '.$_POST["lname"].'!<br>';
+		echo 'Post   method Birthday ==> '.$_POST["date"].'!<br>';
 		echo 'Post  method Email ==> '.$_POST["email"].'!<br>';
-		echo 'Get   method color ==> '.$_POST["color"].'!<br>';
-		echo 'Get  method degree ==> '.$_POST["مدرک"].'!<br>';
-		echo 'Get   method province ==> '.$_POST["ostan"].'!<br>';
-		echo 'Get  method addres ==> '.$_POST["nazar"].'!<br>';
-		echo 'Get   method telephone ==> '.$_POST["telephone"].'!<br>';
-		echo 'Get  method search ==> '.$_POST["search"].'!<br>';
-		echo 'Get   method monthe ==> '.$_POST["monthe"].'!<br>';
+		echo 'Post   method color ==> '.$_POST["color"].'!<br>';
+		echo 'Post  method degree ==> '.$_POST["مدرک"].'!<br>';
+		echo 'Post   method province ==> '.$_POST["ostan"].'!<br>';
+		echo 'Post  method addres ==> '.$_POST["nazar"].'!<br>';
+		echo 'Post   method telephone ==> '.$_POST["telephone"].'!<br>';
+		echo 'Post  method search ==> '.$_POST["search"].'!<br>';
+		echo 'Post   method monthe ==> '.$_POST["monthe"].'!<br>';
 		echo 'Post  method password ==> '.$_POST["password"].'!<br>';
-		echo 'Get   method URL ==> '.$_POST["URL"].'!<br>';
-		echo 'Get  method date ==> '.$_POST["SA"].'!<br>';
-		echo 'Get   method time ==> '.$_POST["time"].'!<br>';
-		echo 'Get  method What is our number ? ==> '.$_POST["quantity"].'!<br>';
-		echo 'Get   method how mach mony do you have : ==> '.$_POST["range"].'!<br>';
-		echo 'Get  method download ==> '.$_POST["file"].'!<br>';
+		echo 'Post   method URL ==> '.$_POST["URL"].'!<br>';
+		echo 'Post  method date ==> '.$_POST["SA"].'!<br>';
+		echo 'Post   method time ==> '.$_POST["time"].'!<br>';
+		echo 'Post  method What is our number ? ==> '.$_POST["quantity"].'!<br>';
+		echo 'Post   method how mach mony do you have : ==> '.$_POST["range"].'!<br>';
+		echo 'Post  method download ==> '.$_POST["file"].'!<br>';
 
 		
 	?> 
